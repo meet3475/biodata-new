16a72376d66c0b4a237531c84685d9399e6e497e
meet3475 <meetdobariya480@gmail.com>
2025-04-30 19:20:15 +0530

    done step section

--- app/page.js
+++ app/page.js
@@ -140,12 +140,7 @@
 //     number: "3",
 //     title: "Get Biodata on Whatsapp",
 //     description: "Enter your Number and get your Biodata PDF on Whatsapp."
-//   },
-//   // {
-//   //   number: "4",
-//   //   title: "Get Biodata on Whatsapp",
-//   //   description: "Enter your Number and get your Biodata PDF on Whatsapp."
-//   // },
+//   }
 // ]
 
 // const problem = [
@@ -796,6 +791,23 @@ import Footer from "@/Layout/Footer/page";
 import Navbar from "@/Layout/Navbar/page";
 import Image from "next/image";
 
+const step = [
+  {
+    number: "1",
+    title: "Add Your Details",
+    description: "Enter all your Personal, Professional and Family details that you want to show on the Biodata."
+  },
+  {
+    number: "2",
+    title: "Choose a Template",
+    description: "Select a biodata design from our curated list of templates."
+  },
+  {
+    number: "3",
+    title: "Get Biodata on Whatsapp",
+    description: "Enter your Number and get your Biodata PDF on Whatsapp."
+  }
+]
 
 export default function Home() {
 
@@ -810,13 +822,13 @@ export default function Home() {
       <div className="relative bg-[#F6F8FF] pt-[120px] pb-[120px] overflow-visible">
         {/* Background Shapes */}
         <div className="absolute top-0 left-0 w-full h-full pointer-events-none z-0">
-          <div className="absolute top-[35%] left-[0%] hidden 2xl:block animate-float-slow">
+          <div className="absolute top-[33%] left-[0%] hidden lg:block animate-float-slow">
             <Image src="/images/shap1.png" alt="shape1" width={150} height={150} />
           </div>
           <div className="absolute bottom-[-15%] right-[0%] animate-float-medium z-10">
             <Image src="/images/shape2.png" alt="shape2" width={200} height={200} />
           </div>
-          <div className="absolute top-[20%] left-[15%] animate-float-fast">
+          <div className="absolute top-[8%] lg:top-[20%] left-[64%] sm:left-[34%] lg:left-[15%] animate-float-fast">
             <Image src="/images/shape3.png" alt="shape3" width={40} height={40} />
           </div>
           <div className="absolute top-[0%] right-[0%] animate-float-xslow">
@@ -847,7 +859,7 @@ export default function Home() {
             </div>
 
             <div className="relative animate-fade-in delay-400">
-              <div className="flex justify-end">
+              <div className="flex justify-center lg:justify-end">
                 <Image src="/images/banner-main.jpg" alt="Hero Main" width={400} height={500} className="rounded-xl" />
               </div>
               <Image
@@ -855,7 +867,7 @@ export default function Home() {
                 alt="camera"
                 width={100}
                 height={100}
-                className="absolute top-0 left-0 -translate-x-1/2 -translate-y-1/2 hidden xl:block animate-float-medium"
+                className="absolute top-0 sm:top-[15%] lg:top-0 left-0 sm:left-[8%] lg:left-0 -translate-x-1/2 -translate-y-1/2 hidden sm:block animate-float-medium"
               />
               <Image
                 src="/images/banner-sm.jpg"
@@ -869,20 +881,59 @@ export default function Home() {
                 alt="md thumb"
                 width={215}
                 height={350}
-                className="absolute bottom-[-7%] left-[-4%] hidden md:block rounded-[40%] animate-float-fast"
+                className="absolute bottom-[-7%] left-[-4%] sm:left-[8%] lg:left-[-4%] hidden md:block rounded-[40%] animate-float-fast"
               />
               <Image
                 src="/images/img-full.png"
                 alt="border full"
                 width={260}
                 height={400}
-                className="absolute bottom-[-9%] left-[-8%] opacity-40 hidden md:block animate-float-fast"
+                className="absolute bottom-[-9%] left-[-8%] sm:left-[5%] lg:left-[-8%] opacity-40 hidden md:block animate-float-fast"
               />
             </div>
           </div>
         </div>
       </div>
       {/* hero section end */}
+
+      {/* Steps to Create Biodata section start */}
+
+      <div className="bg-[#FFFFFF]">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-[80px]">
+          <div className="text-left mb-3">
+            <h6 className="text-[#1b7261]">Our Services</h6>
+            <h2 className="text-[28px] sm:text-[45px] text-[#051145] font-bold">
+              Steps to Create Biodata
+            </h2>
+          </div>
+          <div className="flex flex-col sm:flex-row gap-3">
+            {step.map((item, index) => (
+              <div key={index}>
+                <div className="group transition-all duration-500 transform hover:-translate-y-3 hover:scale-105 hover:shadow-2xl hover:rounded-2xl p-6 sm:p-8 text-left max-w-md mx-auto h-[300px] bg-white">
+                  <div>
+                    <div className="relative w-[100px] h-[100px] flex justify-center items-center text-[#1b7261] text-[32px] font-bold mb-6 overflow-hidden rounded-full bg-[#1B726114]">
+                      <span className="z-10 transition-colors duration-300 group-hover:text-white">
+                        {item.number}
+                      </span>
+                      {/* Background color change with 500ms delay */}
+                      <div className="absolute inset-0 bg-[#1b7261] rounded-full opacity-0 transition-opacity duration-300 delay-200 group-hover:opacity-100"></div>
+                      {/* Circle expansion effect (immediate) */}
+                      <div className="absolute w-0 h-0 bg-[#1b7261] rounded-full transition-all duration-500 group-hover:w-[200px] group-hover:h-[200px] group-hover:-top-10 group-hover:-left-10"></div>
+                    </div>
+                  </div>
+                  <div>
+                    <h3 className="text-[#1b7261] font-bold text-[24px]">
+                      {item.title}
+                    </h3>
+                    <p className="text-[#54595F] text-[16px]">{item.description}</p>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      {/* Steps to Create Biodata section end */}
 
       {/* Footer section start */}
       <Footer />
